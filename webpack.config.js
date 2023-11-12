bbf1e09158948bbfd78d67db68e0522db61642a8
Minhaz <minhaz1217@gmail.com>
2023-11-13 01:50:10 +0600

    combining tailwind in same file working

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,36 @@
-const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HTMLInlineCSSWebpackPlugin =
+  require("html-inline-css-webpack-plugin").default;
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.html',
+  // mode: "production",
+  mode: "development",
+  entry: "./src/app.js",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackPlugin({ template: "./src/index.html", inject: false }),
+    new HTMLInlineCSSWebpackPlugin({
+      replace: {
+        target: '<link rel="stylesheet" href="./output.css" />',
+        removeTarget: true,
+      },
+    }),
+  ],
   module: {
-    rules: [{ test: /\.html$/, use: 'raw-loader' }],
-  },
-  // output: {
-  //   filename: 'main.html',
-  //   path: path.resolve(__dirname, 'dist'),
-  // },
-  devServer: {
-    hot: true,
-    watchFiles: ['src/**/*.html'],
-    static: {
-      directory: path.join(__dirname, 'src'),
-    },
-    compress: false,
-    port: 80,
+    rules: [
+      { test: /\.html$/, use: "raw-loader" },
+      // {
+      //   test: /\.css$/i,
+      //   use: ["style-loader", "css-loader"],
+      // },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
 };
