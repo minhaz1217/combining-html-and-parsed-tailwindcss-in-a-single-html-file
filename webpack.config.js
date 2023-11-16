00af4c9831e8cd8d6a1a57e6d21c1b95981aabba
Minhaz <minhaz1217@gmail.com>
2023-11-17 02:28:27 +0600

    templating working

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HTMLInlineCSSWebpackPlugin =
   require("html-inline-css-webpack-plugin").default;
+const translation = require("./translations/en.json");
 
 module.exports = {
   // mode: "production",
@@ -12,12 +13,21 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
-    new HtmlWebpackPlugin({ template: "./src/index.html", inject: false }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+      description: translation.name,
+    }),
     new HTMLInlineCSSWebpackPlugin(),
   ],
   module: {
     rules: [
-      { test: /\.html$/, use: "raw-loader" },
+      { test: /\.html$/, loader: "underscore-template-loader" },
+      // {
+      //   test: /\.html$/i,
+      //   loader: "html-loader",
+      // },
+      // { test: /\.html$/, use: "raw-loader" },
       // {
       //   test: /\.css$/i,
       //   use: ["style-loader", "css-loader"],
