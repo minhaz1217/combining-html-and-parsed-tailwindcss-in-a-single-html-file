e10add1bbf424b1dfd4309590ce384bf168fb618
Minhaz <minhaz1217@gmail.com>
2023-11-25 21:13:47 +0600

    variable from command line detected

--- webpack.config.js
+++ webpack.config.js
@@ -4,40 +4,45 @@ const HTMLInlineCSSWebpackPlugin =
   require("html-inline-css-webpack-plugin").default;
 const translation = require("./translations/en.json");
 
-module.exports = {
-  // mode: "production",
-  mode: "development",
-  entry: "./src/app.js",
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      inject: true,
-      translations: {
-        ...translation,
-      },
-    }),
-    new HTMLInlineCSSWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      { test: /\.html$/, loader: "underscore-template-loader" },
-      // {
-      //   test: /\.html$/i,
-      //   loader: "html-loader",
-      // },
-      // { test: /\.html$/, use: "raw-loader" },
-      // {
-      //   test: /\.css$/i,
-      //   use: ["style-loader", "css-loader"],
-      // },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
+module.exports = (env) => {
+  console.log(env);
+  console.log(process.argv);
+  return {
+    // mode: "production",
+    mode: "development",
+    entry: "./src/app.js",
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        inject: true,
+        translations: {
+          ...translation,
+          env2: env.tr,
+        },
+      }),
+      new HTMLInlineCSSWebpackPlugin(),
     ],
-  },
+    module: {
+      rules: [
+        { test: /\.html$/, loader: "underscore-template-loader" },
+        // {
+        //   test: /\.html$/i,
+        //   loader: "html-loader",
+        // },
+        // { test: /\.html$/, use: "raw-loader" },
+        // {
+        //   test: /\.css$/i,
+        //   use: ["style-loader", "css-loader"],
+        // },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
+    },
+  };
 };
